3f633cf4c136ddf70afb9777002d217978a1af05
ChangingFond <782773117@qq.com>
2017-11-17 22:01:36 +0800

    绘制流程图

--- PathTest/js/test.js
+++ PathTest/js/test.js
@@ -8,69 +8,241 @@ var range = [];
  @class Edge
  *
  @constructor newRange(min, max)
- */
-var Edge = {
-  newRange: function(min, max) {
-    var edge = {};
-    // 值域最小值
-    edge.min = min;
-    // 值域最大值
-    edge.max = max;
-    // 取值的最小粒度
-    edge.threshold = 1;
-    // 高于值域最大值
-    edge.overMax = function() {
-      if (Number.MAX_VALUE - this.threshold > this.max) {
-        return this.max + this.threshold;
-      } else {
-        return Number.MAX_VALUE;
-      }
-    }
-    // 低于值域最大值
-    edge.belowMax = function() {
-      if (this.max - this.threshold > this.min) {
-        return this.max - this.threshold;
-      } else {
-        return this.min;
-      }
-    }
-    // 值域中值
-    edge.average = function() {
-      return parseInt((this.max + this.min) / 2);
-    }
-    // 高于值域最小值
-    edge.overMin = function() {
-      if (this.min + this.threshold < this.max) {
-        return this.min + this.threshold;
-      } else {
-        return this.max;
-      }
-    }
-    // 低于值域最小值
-    edge.belowMin = function() {
-      if (this.min - this.threshold > 0) {
-        return this.min - this.threshold;
-      } else {
-        return 0;
-      }
-    }
-    // 值域内随机值
-    edge.random = function() {
-      var range = this.max - this.min;
-      var rand = Math.random();
-      return this.min + Math.round(rand * range);
-    }
-    // 判断某一长度是否在值域范围内
-    edge.inRange = function(x) {
-      if (x >= this.min && x <= this.max) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    return edge;
-  }
-}
+*/
+jsPlumb.ready(function () {
+
+   var instance = window.jsp = jsPlumb.getInstance({
+     // default drag options
+     DragOptions: { cursor: 'pointer', zIndex: 2000 },
+     // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+     // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+     ConnectionOverlays: [
+       [ "Arrow", {
+         location: 0.96,
+         visible:true,
+         width:10,
+         length:10,
+         id:"ARROW",
+         events:{
+           // click:function() { alert("you clicked on the arrow overlay")}
+         }
+       } ],
+       [ "Label", {
+         location: 0.4,
+         id: "label",
+         cssClass: "aLabel",
+         events:{
+           // tap:function() { alert("hey"); }
+         }
+       }]
+     ],
+     Container: "canvas"
+   });
+
+   var basicType = {
+     connector: "StateMachine",
+     paintStyle: { stroke: "red", strokeWidth: 4 },
+     hoverPaintStyle: { stroke: "blue" },
+     overlays: [
+       "Arrow"
+     ]
+   };
+   instance.registerConnectionType("basic", basicType);
+
+   // this is the paint style for the connecting lines..
+   var connectorPaintStyle = {
+     strokeWidth: 2,
+     stroke: "#61B7CF",
+     joinstyle: "round",
+     outlineStroke: "white",
+     outlineWidth: 2
+   },
+   // .. and this is the hover style.
+   connectorHoverStyle = {
+     strokeWidth: 3,
+     stroke: "#FF0000",
+     outlineWidth: 5,
+     outlineStroke: "white"
+   },
+   endpointHoverStyle = {
+     fill: "#FF0000",
+     stroke: "#FF0000"
+   },
+   // the definition of source endpoints (the small blue ones)
+   sourceEndpoint = {
+     endpoint: "Dot",
+     paintStyle: {
+       stroke: "#61B7CF",
+       fill: "#61B7CF",
+       radius: 2.5,
+       strokeWidth: 1
+     },
+     maxConnections: -1,
+     isSource: true,
+     connector: ["StateMachine"],
+     connectorStyle: connectorPaintStyle,
+     hoverPaintStyle: endpointHoverStyle,
+     connectorHoverStyle: connectorHoverStyle,
+     dragOptions: {},
+     overlays: [
+       [ "Label", {
+         location: [0.5, 1.5],
+         label: "Drag",
+         cssClass: "endpointSourceLabel",
+         visible:false
+       } ]
+     ]
+   },
+   // the definition of target endpoints (will appear when the user drags a connection)
+   targetEndpoint = {
+     endpoint: "Dot",
+     paintStyle: { fill: "#61B7CF", radius: 2.5 },
+     hoverPaintStyle: endpointHoverStyle,
+     maxConnections: -1,
+     dropOptions: { hoverClass: "hover", activeClass: "active" },
+     isTarget: true,
+     overlays: [
+       [ "Label", { location: [0.5, -0.5], label: "Drop", cssClass: "endpointTargetLabel", visible:false } ]
+     ]
+   },
+   init = function (connection) {
+     connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
+   };
+
+   var _addEndpoints = function (toId, sourceAnchors, targetAnchors) {
+     for (var i = 0; i < sourceAnchors.length; i++) {
+       var sourceUUID = toId + sourceAnchors[i];
+       instance.addEndpoint(toId, sourceEndpoint, {
+         anchor: sourceAnchors[i], uuid: sourceUUID
+       });
+     }
+     for (var j = 0; j < targetAnchors.length; j++) {
+       var targetUUID = toId + targetAnchors[j];
+       instance.addEndpoint(toId, targetEndpoint, { anchor: targetAnchors[j], uuid: targetUUID });
+     }
+   };
+
+   // suspend drawing and initialise.
+   instance.batch(function () {
+
+     _addEndpoints("node-input", ["BottomCenter"], []);
+     _addEndpoints("node-init", ["BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node1", ["BottomCenter","RightMiddle"], ["TopCenter"]);
+     _addEndpoints("node2", ["BottomCenter"], ["LeftMiddle"]);
+     _addEndpoints("node3", ["BottomCenter","RightMiddle"], ["TopCenter"]);
+     _addEndpoints("node4", ["BottomCenter"], ["LeftMiddle"]);
+     _addEndpoints("node5", ["BottomCenter","RightMiddle"], ["TopCenter"]);
+     _addEndpoints("node6", ["BottomCenter"], ["LeftMiddle"]);
+     _addEndpoints("node7", ["LeftMiddle","RightMiddle"], ["TopCenter"]);
+     _addEndpoints("node8", ["LeftMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node9", ["LeftMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node10", ["LeftMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node11", [], ["TopCenter"]);
+     _addEndpoints("node12", [], ["TopCenter"]);
+     _addEndpoints("node13", ["LeftMiddle","RightMiddle"], ["TopCenter"]);
+     _addEndpoints("node14", ["RightMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node15", [], ["TopCenter"]);
+     _addEndpoints("node16", ["RightMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node17", ["RightMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node18", ["RightMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node19", ["RightMiddle","BottomCenter"], ["TopCenter"]);
+     _addEndpoints("node20", [], ["TopCenter"]);
+     // listen for new connections; initialise them the same way we initialise the connections at startup.
+     instance.bind("connection", function (connInfo, originalEvent) {
+       init(connInfo.connection);
+     });
+
+     // make all the window divs draggable
+     // instance.draggable(jsPlumb.getSelector(".flowchart-demo .window"), { grid: [20, 20] });
+     // THIS DEMO ONLY USES getSelector FOR CONVENIENCE. Use your library's appropriate selector
+     // method, or document.querySelectorAll:
+     //jsPlumb.draggable(document.querySelectorAll(".window"), { grid: [20, 20] });
+
+     // connect a few up
+     c_nodeinput_nodeinit = instance.connect({uuids: ["node-inputBottomCenter", "node-initTopCenter"]});
+     c_nodeinit_node1 = instance.connect({uuids: ["node-initBottomCenter", "node1TopCenter"]});
+     c_node1_node2 = instance.connect({uuids: ["node1RightMiddle", "node2LeftMiddle"]});
+     c_node1_node2.getOverlay("label").setLabel('Y');
+     c_node1_node3 = instance.connect({uuids: ["node1BottomCenter", "node3TopCenter"]});
+     c_node1_node3.getOverlay("label").setLabel('N');
+     c_node2_node3 = instance.connect({uuids: ["node2BottomCenter", "node3TopCenter"]});
+     c_node3_node4 = instance.connect({uuids: ["node3RightMiddle", "node4LeftMiddle"]});
+     c_node3_node4.getOverlay("label").setLabel('Y');
+     c_node3_node5 = instance.connect({uuids: ["node3BottomCenter", "node5TopCenter"]});
+     c_node3_node5.getOverlay("label").setLabel('N');
+     c_node4_node5 = instance.connect({uuids: ["node4BottomCenter", "node5TopCenter"]});
+     c_node5_node6 = instance.connect({uuids: ["node5RightMiddle", "node6LeftMiddle"]});
+     c_node5_node6.getOverlay("label").setLabel('Y');
+     c_node5_node7 = instance.connect({uuids: ["node5BottomCenter", "node7TopCenter"]});
+     c_node5_node7.getOverlay("label").setLabel('N');
+     c_node6_node7 = instance.connect({uuids: ["node6BottomCenter", "node7TopCenter"]});
+     c_node7_node8 = instance.connect({uuids: ["node7RightMiddle", "node8TopCenter"]});
+     c_node7_node8.getOverlay("label").setLabel('Y');
+     c_node8_node9 = instance.connect({uuids: ["node8BottomCenter", "node9TopCenter"]});
+     c_node8_node9.getOverlay("label").setLabel('N');
+     c_node8_node12 = instance.connect({uuids: ["node8LeftMiddle", "node12TopCenter"]});
+     c_node8_node12.getOverlay("label").setLabel('Y');
+     c_node9_node10 = instance.connect({uuids: ["node9BottomCenter", "node10TopCenter"]});
+     c_node9_node10.getOverlay("label").setLabel('N');
+     c_node9_node12 = instance.connect({uuids: ["node9LeftMiddle", "node12TopCenter"]});
+     c_node9_node12.getOverlay("label").setLabel('Y');
+     c_node10_node11 = instance.connect({uuids: ["node10BottomCenter", "node11TopCenter"]});
+     c_node10_node11.getOverlay("label").setLabel('N');
+     c_node10_node12 = instance.connect({uuids: ["node10LeftMiddle", "node12TopCenter"]});
+     c_node10_node12.getOverlay("label").setLabel('Y');
+     c_node7_node13 = instance.connect({uuids: ["node7LeftMiddle", "node13TopCenter"]});
+     c_node7_node13.getOverlay("label").setLabel('N');
+     c_node13_node14 = instance.connect({uuids: ["node13RightMiddle", "node14TopCenter"]});
+     c_node13_node14.getOverlay("label").setLabel('Y');
+     c_node14_node12 = instance.connect({uuids: ["node14RightMiddle", "node12TopCenter"]});
+     c_node14_node12.getOverlay("label").setLabel('Y');
+     c_node14_node15 = instance.connect({uuids: ["node14BottomCenter", "node15TopCenter"]});
+     c_node14_node15.getOverlay("label").setLabel('N');
+     c_node16_node17 = instance.connect({uuids: ["node16RightMiddle", "node17TopCenter"]});
+     c_node16_node17.getOverlay("label").setLabel('Y');
+     c_node17_node12 = instance.connect({uuids: ["node17RightMiddle", "node12TopCenter"]});
+     c_node17_node12.getOverlay("label").setLabel('Y');
+     c_node17_node15 = instance.connect({uuids: ["node17BottomCenter", "node15TopCenter"]});
+     c_node17_node15.getOverlay("label").setLabel('N');
+     c_node13_node16 = instance.connect({uuids: ["node13LeftMiddle", "node16TopCenter"]});
+     c_node13_node16.getOverlay("label").setLabel('N');
+     c_node16_node18 = instance.connect({uuids: ["node16BottomCenter", "node18TopCenter"]});
+     c_node16_node18.getOverlay("label").setLabel('N');
+     c_node17_node15 = instance.connect({uuids: ["node17BottomCenter", "node15TopCenter"]});
+     c_node17_node15.getOverlay("label").setLabel('N');
+     c_node18_node19 = instance.connect({uuids: ["node18RightMiddle", "node19TopCenter"]});
+     c_node18_node19.getOverlay("label").setLabel('Y');
+     c_node18_node20 = instance.connect({uuids: ["node18BottomCenter", "node20TopCenter"]});
+     c_node18_node20.getOverlay("label").setLabel('N');
+     c_node19_node12 = instance.connect({uuids: ["node19RightMiddle", "node12TopCenter"]});
+     c_node19_node12.getOverlay("label").setLabel('Y');
+     c_node19_node15 = instance.connect({uuids: ["node19BottomCenter", "node15TopCenter"]});
+     c_node19_node15.getOverlay("label").setLabel('N');
+     //
+     // listen for clicks on connections, and offer to delete connections on click.
+     //
+     // instance.bind("click", function (conn, originalEvent) {
+     //    // if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
+     //      //   instance.detach(conn);
+     //     conn.toggleType("basic");
+     // });
+
+     instance.bind("connectionDrag", function (connection) {
+       console.log("connection " + connection.id + " is being dragged. suspendedElement is ", connection.suspendedElement, " of type ", connection.suspendedElementType);
+     });
+
+     instance.bind("connectionDragStop", function (connection) {
+       console.log("connection " + connection.id + " was dragged");
+     });
+
+     instance.bind("connectionMoved", function (params) {
+       console.log("connection " + params.connection.id + " was moved");
+     });
+   });
+
+   jsPlumb.fire("jsPlumbDemoLoaded", instance);
+
+});
 
 /**
  *
@@ -80,7 +252,7 @@ var Edge = {
  @class Triangle
  *
  @constructor newTriangle(a, b, c)
- */
+*/
 var Triangle = {
   getTriangleType: function(a, b, c) {
     if (!(range[0].inRange(a) && range[1].inRange(b) && range[2].inRange(c))) {
